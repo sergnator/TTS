0e85795ce6ba7447b5f7576e9bc35e571433c803
sergn <d@ma.com>
2024-11-16 22:04:17 +0300

    version ультро ебейшая версия

--- mainserver.py
+++ mainserver.py
@@ -12,7 +12,7 @@ app.config["SECRET_KEY!"] = "slava_jesusu"
 soketio = SocketIO(app)
 
 datas = {}
-model = Model(model_path="/vosk-model-ru-0.42.zip")
+model = Model("vosk-model-small-ru-0.22")
 
 
 @soketio.on("message")
@@ -23,9 +23,12 @@ def handle_message(message):
 @soketio.on("command")
 def handle_command(message):
     offline_recognizer = KaldiRecognizer(model, FRT)
-    offline_recognizer.AcceptWaveform(datas[request.sid]["speech"])
-    text = json.loads(offline_recognizer.Result())["text"]
-    print(text)
+    for el in datas[request.sid]["speech"]:
+        if offline_recognizer.AcceptWaveform(el):
+            text = json.loads(offline_recognizer.Result())["text"]
+            print(text)
+        else:
+            print(offline_recognizer.PartialResult())
     datas[request.sid]["speech"] = []
 
 
